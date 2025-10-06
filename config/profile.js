9d602c155b8d8d481311b0993f3d69ddf9d392cd
Jeff Blankenburg <github@jeffblankenburg.com>
2025-10-06 10:20:01 -0400

    Update profile.js

--- config/profile.js
+++ config/profile.js
@@ -1,21 +1,20 @@
-export const artOrder = [ "avatar", "banner" ];
+export const artOrder = [ "banner" ];
 
 export const profile = {
-	name: "DAVID NEAL",
-	handle: "reverentgeek",
-	bio: "David is a family man, geek, musician, illustrator, speaker, software developer, and Microsoft MVP living in North GA. He runs on a high-octane mixture of caffeine and JavaScript, and is entirely made of bacon.",
+	name: "Jeff Blankenburg",
+	handle: "jeffblankenburg",
+	bio: "Building cool things and sharing them with others. Husband, father, speaker, technophile, golfer, and unapologetic collector. Powered by Red Bull, curiosity, and a love for code, community, and AI.",
 	infoSections: [
-		{ label: "Work", value: [ "Developer Relations Engineer", "https://plaid.com" ], style: "gradient", leadingNewline: true },
-		{ label: "Handle", value: "reverentgeek", style: "yellow" },
-		{ label: "Portfolio", value: "https://reverentgeek.com", style: "yellow" },
-		{ label: "Email", value: "david@reverentgeek.com", style: "yellow" },
-		{ label: "LinkedIn", value: "https://linkedin.com/in/davidneal", style: "yellow" },
-		{ label: "GitHub", value: "https://github.com/reverentgeek", style: "yellow" },
-		{ label: "Instagram", value: "https://instagram.com/reverentgeek", style: "yellow" },
-		{ label: "Mastodon", value: "@reverentgeek@reverentgeek.com", style: "yellow" },
-		{ label: "BlueSky", value: "@reverentgeek.com", style: "yellow" },
-		{ label: "Threads", value: "@reverentgeek", style: "yellow" },
-		{ label: "X", value: "https://x.com/reverentgeek", style: "yellow" },
-		{ label: "Card", value: "npx reverentgeek", style: "orange", leadingNewline: true }
+		{ label: "Work", value: [ "Principal Developer Advocate", "https://dynatrace.com" ], style: "gradient", leadingNewline: true },
+		{ label: "Handle", value: "jeffblankenburg", style: "yellow" },
+		{ label: "Portfolio", value: "https://jeffblankenburg.info", style: "yellow" },
+		{ label: "Email", value: "jeff@jeffblankenburg.com", style: "yellow" },
+		{ label: "LinkedIn", value: "https://linkedin.com/in/jeffblankenburg", style: "yellow" },
+		{ label: "GitHub", value: "https://github.com/jeffblankenburg", style: "yellow" },
+		{ label: "Instagram", value: "https://instagram.com/jeffblankenburg", style: "yellow" },
+		{ label: "BlueSky", value: "@jeffblankenburg.com", style: "yellow" },
+		{ label: "Threads", value: "@jeffblankenburg", style: "yellow" },
+		{ label: "X", value: "https://x.com/jeffblankenburg", style: "yellow" },
+		{ label: "Card", value: "npx jeffblankenburg", style: "orange", leadingNewline: true }
 	]
 };
